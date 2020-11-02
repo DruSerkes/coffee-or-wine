7690cbe9a2c4d8365beecec5e81c84fab6da26c3
Dru Serkes <DruSerkes@gmail.com>
2020-11-02 01:23:07 -0500

    add helmet, morgan

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app: express.Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(helmet());
+app.use(morgan('dev'));
 
 app.get('/', (req, res, next) => {
     res.send("Welcome to Coffee or Wine");
