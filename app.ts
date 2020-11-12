0e6385a7afd43f9c3751ba91eb94d3d4c9f432d3
Dru Serkes <DruSerkes@gmail.com>
2020-11-12 16:30:07 -0500

    add nunjucks, styles.css

--- app.ts
+++ app.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import nunjucks from 'nunjucks';
 // import axios from 'axios';
 // import { API_KEY } from './config';
 
 // const BASE_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
 
 const app: express.Application = express();
+
+nunjucks.configure('templates', {
+    autoescape: true,
+    express: app,
+})
 
 app.use(express.json());
 app.use(express.urlencoded());
