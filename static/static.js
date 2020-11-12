951df6f285e534d5412a9d0f1ad61698d4e77dcd
Dru Serkes <DruSerkes@gmail.com>
2020-11-12 16:16:30 -0500

    refactor for es6

--- static/static.js
+++ static/static.js
@@ -2,30 +2,21 @@ const coffeeBtn = document.querySelector('.Btn-Coffee');
 const wineBtn = document.querySelector('.Btn-Wine');
 let latitude, longitude, error;
 
-function locationSuccess(position) {
-    console.log(position.coords);
+const locationSuccess = (position) => {
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
-    console.log('latitude = ', latitude);
-    console.log('longitude = ', longitude);
-}
+};
 
-function locationFail(error) {
+const locationFail = (error) => {
     console.log({ error });
     window.location.reload();
-}
+};
 
-function getCoords() {
-    navigator.geolocation.getCurrentPosition(locationSuccess, locationFail);
-}
+const getCoords = () => (navigator.geolocation.getCurrentPosition(locationSuccess, locationFail));
 
-function handleGetCoffee() {
-    window.location.assign(`https://maps.google.com/?ll=${latitude},${longitude}&q=coffee&z=15`);
-}
+const handleGetCoffee = () => (window.location.assign(`https://maps.google.com/?ll=${latitude},${longitude}&q=coffee&z=15`));
 
-function handleGetWine() {
-    window.location.assign(`https://maps.google.com/?ll=${latitude},${longitude}&q=wine+bar&z=15`);
-}
+const handleGetWine = () => (window.location.assign(`https://maps.google.com/?ll=${latitude},${longitude}&q=wine+bar&z=15`));
 
 coffeeBtn?.addEventListener('click', handleGetCoffee);
 wineBtn?.addEventListener('click', handleGetWine);
